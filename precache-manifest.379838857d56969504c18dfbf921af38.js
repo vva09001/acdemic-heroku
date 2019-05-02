e7cf3de3ad5ea3090f4cf80c97b4165680ecbb04
vva0901 <haidangvu07@gmail.com>
2019-05-02 23:45:59 +0700

    Updates

---
+++ precache-manifest.379838857d56969504c18dfbf921af38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ea4c43614e5b0beff1d450e9d280ea2",
+    "url": "/acdemic-heroku/static/media/Search.2ea4c436.svg"
+  },
+  {
+    "revision": "93998245429b2bddf3885d142fa6a0dd",
+    "url": "/acdemic-heroku/static/media/Delete.93998245.svg"
+  },
+  {
+    "revision": "2a75a6ba5700d239a4ac",
+    "url": "/acdemic-heroku/static/js/runtime~main.7fbd8f4a.js"
+  },
+  {
+    "revision": "c98698110f883c1020d9",
+    "url": "/acdemic-heroku/static/js/main.60781707.chunk.js"
+  },
+  {
+    "revision": "f19b097989f04838a05d",
+    "url": "/acdemic-heroku/static/js/2.96fa56d8.chunk.js"
+  },
+  {
+    "revision": "c98698110f883c1020d9",
+    "url": "/acdemic-heroku/static/css/main.e7f59eba.chunk.css"
+  },
+  {
+    "revision": "f19b097989f04838a05d",
+    "url": "/acdemic-heroku/static/css/2.29a47821.chunk.css"
+  },
+  {
+    "revision": "3131014ec8fa971feae5451092e24dc1",
+    "url": "/acdemic-heroku/index.html"
+  }
+];
